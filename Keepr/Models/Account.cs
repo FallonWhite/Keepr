9bacaae9d37a57cf66f14e027cfb999ebd180c94
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-09-13 14:26:53 -0600

    addedTransientsTpStartup.cs

--- Keepr/Models/Account.cs
+++ Keepr/Models/Account.cs
@@ -4,10 +4,10 @@ namespace Keepr.Models
 {
   public class Account : Profile
   {
-    public new string Id { get; set; }
-    public new string Name { get; set; }
-    public new string Email { get; set; }
-    public new string Picture { get; set; }
+    // public new string Id { get; set; }
+    // public new string Name { get; set; }
+    // public new string Email { get; set; }
+    // public new string Picture { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
   }
