35d979e806cb3502610ded417e3313b02dfbd64c
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-09-15 11:38:22 -0600

    BreakTime

--- Keepr.client/src/pages/ProfilePage.vue
+++ Keepr.client/src/pages/ProfilePage.vue
@@ -6,36 +6,68 @@
       </div>
       <div class="col-md-10">
         <h3 class="shadow">
-          {{ profile.name }}
+          {{ state.profile.name }}
         </h3>
         <h5 class="shadow">
-          Vaults: {{ vaults.length }}
+          Vaults: {{ state.vaults.length }}
         </h5>
         <h5 class="shadow">
           Keeps: {{ state.keeps.length }}
         </h5>
       </div>
     </div>
+    <div class="row m-3">
+      <h2>Vaults</h2>
+      <button class="btn btn-outline-maroon" title="Create Vault" v-if="state.account.id === state.profile.id" data-toggle="modal" data-target="#createVaultModal">
+        <i class="mdi mdi-24px mdi-plus"></i>
+      </button>
+    </div>
+    <div class="row m-1">
+      <Vault v-for="vault in state.vaults" :key="vault.id" :vault="vault" />
+    </div>
+    <div class="row m-3">
+      <h2>Keeps</h2>
+      <button class="btn btn-outline-maroon" title="Create Keep" v-if="state.account.id === state.profile.id" data-toggle="modal" data-target="#createKeepModal">
+        <i class="mdi mdi-24px mdi-plus"></i>
+      </button>
+    </div>
+    <div class="masonry flex-wrap m-2">
+      <Keep v-for="k in keeps" :key="k.id" :keep="k" class="keep" />
+    </div>
   </div>
 </template>
 
 <script>
-import { computed, onMounted } from '@vue/runtime-core'
+import { computed, onMounted, reactive } from '@vue/runtime-core'
 import { keepsService } from '../services/KeepsService'
+import { vaultsService } from '../services/VaultsService'
+import { profilesService } from '../services/ProfilesService'
 import Pop from '../utils/Notifier'
 import { useRoute } from 'vue-router'
 import { AppState } from '../AppState'
 export default {
   setup() {
-    const router = useRoute()
+    const route = useRoute()
+    const state = reactive({
+      user: computed(() => AppState.user),
+      profile: computed(() => AppState.activeProfile),
+      keeps: computed(() => AppState.keeps),
+      vaults: computed(() => AppState.vaults)
+      // account: computed(() => AppState.account) I shouldn't need account bc I'm using profile but I'm leaving it in case.
+    })
     onMounted(async() => {
       try {
-        await keepsService.getAll({ creatorId: router.params.id })
+        await keepsService.getKeepsByProfileId(route.params.id)
+        await vaultsService.getVaultsByProfileId(route.params.id)
+        await profilesService.getProfileById(route.params.id)
       } catch (error) {
         Pop.toast(error, 'error')
       }
     })
     return {
+      state,
+      profile: computed(() => AppState.profile),
+      vaults: computed(() => AppState.vaults),
       keeps: computed(() => AppState.keeps)
     }
   }
