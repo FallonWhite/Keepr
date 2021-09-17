a7a9854dbe1ab1d3352bf9996d4e439d1b0fe8e6
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-09-17 12:08:00 -0600

    VaultCompToTest- Basic

--- Keepr.client/src/components/Vault.vue
+++ Keepr.client/src/components/Vault.vue
@@ -1,18 +1,91 @@
 <template>
-  <div class="vault">
+  <div v-if="vault.isPrivate == false" class="m-1 vault-bgimg">
+    <router-link :to="{name: 'Vault', params: {id: vault.id}}" @click="setActiveVault">
+      <div class="Vault card m-2 shadow">
+        <div class="my-2 p-2">
+          <h5 class="vault-name text-primary text-center">
+          </h5>
+        </div>
+      </div>
+    </router-link>
+
+    <div>
+      <p>{{ vault.name }}</p>
+    </div>
+
+    <div class="d-flex justify-content-center">
+      <button @click="deleteVault(vault.id)" v-if="vault.creatorId === account.id" class="btn btn-danger shadow" title="Delete Vault">
+        Delete
+      </button>
+    </div>
+  </div>
+
+  <div v-if="vault.isPrivate === true" class="vault-bg m-1">
+    <router-link :to="{name: 'Vault', params: {id: vault.id}}" @click="setActiveVault">
+      <div class="Vault card m-2 shadow">
+        <div class="my-2 p-2">
+          <div class="vault-name text-center text-warning">
+            <h5>Private Vault</h5>
+          </div>
+        </div>
+      </div>
+    </router-link>
+
+    <div>
+      <h5>{{ vault.name }}</h5>
+    </div>
+
+    <div class="d-flex justify-content-center">
+      <button @click="deleteVault(vault.id)" v-if="vault.creatorId === account.id" class="btn btn-outline-dark btn-danger shadow" title="Delete Vault">
+        Delete
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+
+import { computed, reactive } from '@vue/runtime-core'
+import { vaultsService } from '../services/VaultsService'
+import { AppState } from '../AppState'
+
 export default {
-  name: 'Vault',
-  setup() {
-    return {}
+
+  props: {
+    vault: { type: Object, required: true }
   },
-  components: {}
+
+  setup(props) {
+    const state = reactive({
+    })
+    return {
+      state,
+      account: computed(() => AppState.account),
+      async deleteVault(id) {
+        if (confirm('Are you sure?')) {
+          await vaultsService.deleteVault(id)
+        }
+      },
+
+      setActiveVault() {
+        vaultsService.setActiveVault(props.vault.id)
+      }
+    }
+  }
+
 }
+
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+
+.Vault {
+ background-image: url("../assets/img/vault.png");
+ background-size: cover;
+}
+
+.vault-bgimg:hover {
+ transform: scale(1.1);
+ }
 
 </style>
