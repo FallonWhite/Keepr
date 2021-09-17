ff55cc8efac99c8a87ab9f9817e439576a8c3cf9
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-09-17 08:18:13 -0600

    Debug

--- Keepr/Models/VaultKeep.cs
+++ Keepr/Models/VaultKeep.cs
@@ -1,12 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 namespace Keepr.Models
 {
   public class VaultKeep
   {
-    public int Id { get; set; }
-    public string CreatorId { get; set; }
-    public int VaultId { get; set; }
-    public int KeepId { get; set; }
     public Vault vault { get; set; }
     public Keep keep { get; set; }
+    public int Id { get; set; }
+    public string CreatorId { get; set; }
+    [Required]
+    public int VaultId { get; set; }
+    public int KeepId { get; set; }
   }
 }
